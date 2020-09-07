ec49311b7d80c36c9cb1d264321adc4760708740
Tharidu Ishara <tharindujayalath79@gmail.com>
2020-09-07 21:35:46 +0530

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
@@ -23,6 +22,7 @@ class App extends Component{
             </div>
           </Navbar>
           <Menu dishes={this.state.dishes}/>
+           
         </div>
     );
   }
